58201772ee7a2bc7654bf081eb6618e7fb195ddf
Awais <muhammadawaisidrees478@gmail.com>
2025-08-11 14:12:12 +0500

    project10(Basic Authentication and Permission Class)

---
+++ project10/project10/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from p2 import views
+from rest_framework.routers import DefaultRouter
+#create router
+router=DefaultRouter()
+#Register StudentViewSet with router
+router.register('studentapi',views.StudentModelViewSet,basename='Student')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+]
+
