e67340275f137519a1ee84ac1cc019446cf78f12
Awais <muhammadawaisidrees478@gmail.com>
2025-08-12 10:44:29 +0500

    project11(Session Authentication and Permission)

--- project10/p2/views.py
+++ project10/p2/views.py
@@ -1,11 +1,11 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
 from rest_framework.authentication import BasicAuthentication
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAdminUser]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [AllowAny]
 
