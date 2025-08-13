27e8c02ea24a1e3186e5c0abf8ba4f9a51f1b8d2
Awais <muhammadawaisidrees478@gmail.com>
2025-08-13 11:39:52 +0500

    project16(Permission Classes,httpie,use of Token Authentication)

---
+++ project16/p2/views.py
@@ -0,0 +1,12 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.authentication import TokenAuthentication
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
